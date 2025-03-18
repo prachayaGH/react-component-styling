3cfec6b9551d982e46c66c9f8b0f24c5a18a467d
unknown <Prachaya>
2025-03-18 14:21:14 +0700

    feat: component styling

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,29 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Alert() {}
+function Alert({type,text}) {
+    const  defaultStyle = css` 
+        background-color: #f8d7da; 
+        color: #721c24; 
+        padding: 10px; 
+        margin: 10px 0;`
+    const error = css`
+        background-color: #DE5753;
+        color: black`
+    const warning = css`
+        background-color:rgb(222, 146, 83);
+        color: black`
+    const info = css`
+        background-color: rgb(222, 201, 83);
+        color: black`
+    const success = css`
+        background-color: #26B795;
+        color: black`
+    return (
+        <div css={[defaultStyle,type === 'error' ? error : type === 'warning' ? warning : type === 'info' ? info : success]}>{text}</div>
+    )
+}
+
+export default Alert;
+
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,21 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+
+function Button({type,text}) {
+    const defaultStyle = css`
+        background-color: black; 
+        color: white; 
+        padding: 15px 50px; 
+        margin: 10px; `
+    const primary = css`
+        background-color: #074EE8;`
+    const secondary = css`
+        background-color: #07A4E8;`
+    return (
+        <button css={[defaultStyle,type === 'primary' ? primary : secondary]}>{text}</button>
+    )
+}
+
+export default Button;
